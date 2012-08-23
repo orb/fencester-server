84167c9c0505bb61d903e98cf389261167200216
Shad Gregory <captshadg@gmail.com>
2012-08-23 13:02:24 -0500

    another update

--- project.clj
+++ project.clj
@@ -6,7 +6,6 @@
                  [hiccup "1.0.1"]
                  [compojure "1.1.1"]]
   :plugins [[lein-ring "0.7.1"]
-            [org.clojure/tools.nrepl "0.2.0-beta9"]
             [lein-swank "1.4.2"]]
   :ring {:handler fencester-server.handler/app}
   :dev-dependencies [[ring-mock "0.1.2"]])
