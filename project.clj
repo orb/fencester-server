17ee69fab8d0abb2aec8b3ab6b73a70a539331c1
Shad Gregory <captshadg@gmail.com>
2012-08-23 14:37:21 -0500

    added clj-http

--- project.clj
+++ project.clj
@@ -4,6 +4,7 @@
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [cheshire "4.0.1"]
                  [hiccup "1.0.1"]
+                 [clj-http "0.5.3"]
                  [compojure "1.1.1"]]
   :plugins [[lein-ring "0.7.1"]
             [lein-swank "1.4.2"]]
